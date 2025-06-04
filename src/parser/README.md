0f138b49dce8c3581473350daf761c16c01d2b7f
Ferdi K <ferdi.kossmann@outlook.com>
2025-06-04 15:30:48 -0400

    checkpoint before pyre

---
+++ src/parser/README.md
@@ -0,0 +1,7 @@
+
+
+Pyre has some weird thing where you need to do:
+
+`export PYRE_VERSION=client_and_binary`
+
+You can then do `pyre init` to set up a config.
---
+++ src/parser/static_analysis.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+class StaticGraph:
+
+    def __init__(self):
+        self.G = None
+        pass
+
+
+    def parse_graph(self, in_path, cache_path):
