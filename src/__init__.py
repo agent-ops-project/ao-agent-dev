4d5975f8cc77bf21f6462ae85ea6a5e0708d5d80
Ferdi K <ferdi.kossmann@outlook.com>
2025-09-26 00:55:22 -0400

    [misc] minor

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 from aco.runner.context_manager import aco_launch as launch, log
-from aco.runner.bird_utils import patched_call
 from aco.runner.taint_wrappers import get_taint_origins, taint_wrap, untaint_if_needed
 
 
-__all__ = ["launch", "log", "patched_call", "untaint_if_needed", "get_taint_origins", "taint_wrap"]
+__all__ = ["launch", "log", "untaint_if_needed", "get_taint_origins", "taint_wrap"]
